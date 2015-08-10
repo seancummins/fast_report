11c247e01cb28b39607fb64aba5ac8c367596c1c
Sean Cummins <sean@scummins.com>
2015-08-10 13:49:26 -0400

    Updated shebang for Python3

--- fastvp_report.py
+++ fastvp_report.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 fastvp_report.py - Reports per-device Symmetrix FASTVP policies & associations, capacity, and binding information
